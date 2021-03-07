02c1ed293eaedcd673de0cc80a7052a0bab85ee2
Travis Fischer <fisch0920@gmail.com>
2021-03-07 18:02:08 -0500

    😯

--- packages/react-static-tweets/src/twitter.tsx
+++ packages/react-static-tweets/src/twitter.tsx
@@ -16,6 +16,11 @@ export type TweetAstMap = {
   [tweetId: string]: TweetAst
 }
 
+export interface TwitterContextProviderProps {
+  value: Partial<TwitterContextValue>
+  children?: ReactNode
+}
+
 // Saves the tweets returned as props to the page
 const TwitterContext = createContext<TwitterContextValue>({
   tweetAstMap: {},
@@ -31,27 +36,26 @@ export function useTwitterContext() {
   return useContext(TwitterContext)
 }
 
-type TwitterContextProviderProps = {
-  value: Partial<TwitterContextValue>
-  children?: ReactNode
-}
-
 // allows partials that override outer providers
 export function TwitterContextProvider({
   value,
   children
 }: TwitterContextProviderProps) {
   const currentContext = useContext(TwitterContext)
+  const { tweetAstMap, swrOptions, ...rest } = value
   const mergedContext = {
+    ...currentContext,
+    ...rest,
     tweetAstMap: {
-      ...value.tweetAstMap,
-      ...currentContext.tweetAstMap
+      ...currentContext.tweetAstMap,
+      ...tweetAstMap
     },
     swrOptions: {
-      ...value.swrOptions,
-      ...currentContext.swrOptions
+      ...currentContext.swrOptions,
+      ...swrOptions
     }
   }
+
   return (
     <TwitterContext.Provider value={mergedContext}>
       {children}
